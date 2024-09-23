5cfa0c84d39540ca834de51d567c732bf7e02d5a
THRISHA S <126604173+thrisha1012@users.noreply.github.com>
2024-09-23 12:01:46 +0530

    Time: 5 ms (57.28%), Space: 43.9 MB (51.85%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,28 +1,20 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        int[] primes = {2, 3, 5};  // Initialize the primes array
-        int[] indices = {0, 0, 0}; // Initialize indices for multiples of 2, 3, 5
-        List<Integer> uglyArr = new ArrayList<>();  // Initialize the ugly number array with 1
-        uglyArr.add(1);
+        List<Integer>list=new ArrayList<>();
+        list.add(1);
+        int i2=0,i3=0,i5=0;
+        while(list.size()<n){
+            int next2=list.get(i2)*2;
+            int next3=list.get(i3)*3;
+            int next5=list.get(i5)*5;
+            int next=Math.min(next2,Math.min(next3,next5));
 
-        for (int i = 1; i < n; ++i) {
-            // Calculate the next possible ugly numbers
-            int[] nextUglies = {
-                uglyArr.get(indices[0]) * primes[0],
-                uglyArr.get(indices[1]) * primes[1],
-                uglyArr.get(indices[2]) * primes[2]
-            };
-            int minValue = Math.min(nextUglies[0], Math.min(nextUglies[1], nextUglies[2]));  // Find the smallest value
-            uglyArr.add(minValue);  // Append the smallest value to uglyArr
+            if(next==next2) i2++;
+            if(next==next3) i3++;
+            if(next==next5) i5++;
 
-            // Update indices for primes that generated the current min_value
-            for (int j = 0; j < 3; ++j) {
-                if (nextUglies[j] == minValue) {
-                    indices[j]++;
-                }
-            }
+            list.add(next);
         }
-
-        return uglyArr.get(n - 1);
+        return list.get(n-1);
     }
 }
