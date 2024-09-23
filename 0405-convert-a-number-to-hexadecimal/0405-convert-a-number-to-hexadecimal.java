3e0dc3a1210ad10aaa8133cf3dddf6082ce339c0
THRISHA S <126604173+thrisha1012@users.noreply.github.com>
2024-09-23 10:26:00 +0530

    Time: 0 ms (100%), Space: 40.5 MB (94.93%) - LeetHub

--- 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
+++ 0405-convert-a-number-to-hexadecimal/0405-convert-a-number-to-hexadecimal.java
@@ -1,15 +1,16 @@
 class Solution {
     public String toHex(int num) {
-        StringBuilder sb=new StringBuilder();
-        char hex[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
-        if(num==0) return "0";
+        // StringBuilder sb=new StringBuilder();
+        // char hex[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+        // if(num==0) return "0";
         
-            long digit=num & 0xFFFFFFFFL;
-            while(digit>0){
-            sb.append(hex[(int)(digit%16)]);
-            digit=digit/16;
-        }
-        return sb.reverse().toString();
+        //     long digit=num & 0xFFFFFFFFL;
+        //     while(digit>0){
+        //     sb.append(hex[(int)(digit%16)]);
+        //     digit=digit/16;
+        // }
+        // return sb.reverse().toString();
+        return Integer.toHexString(num);
         
     }
 }
