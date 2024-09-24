0797e522610f9b5f467c7bc73da29b49bef76a93
THRISHA S <126604173+thrisha1012@users.noreply.github.com>
2024-09-24 14:42:02 +0530

    Time: 2 ms (57.93%), Space: 42.1 MB (73.04%) - LeetHub

---
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int res[]=new int[code.length];
+        int n=code.length;
+        for(int i=0;i<n;i++){
+            if(k==0){
+                return res;
+            }
+            else if(k>0){
+                for(i=0;i<n;i++){
+                    int s=0;
+                    for(int j=1;j<=k;j++){
+                        s=s+code[(i+j)%n];
+                    }
+                    res[i]=s;
+                }
+            }
+            else if(k<0){
+                for(i=0;i<n;i++){
+                    int s=0;
+                    for(int j=1;j<=-1*k;j++){
+                        s=s+code[(i-j+n)%n];
+                    }
+                    res[i]=s;
+                }
+                                                          
+            }
+            
+        }
+        return res;
+        
+    }
+}
