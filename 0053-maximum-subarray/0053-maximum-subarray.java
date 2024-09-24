8c66528590b0f3f5fef6d8f5b3c870d7e3cd1031
THRISHA S <126604173+thrisha1012@users.noreply.github.com>
2024-09-24 11:59:38 +0530

    Time: 1 ms (99.39%), Space: 57.1 MB (43.44%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,8 +5,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             sum=sum+nums[i];
             if(sum>max) max=sum;
-            if(sum<0) sum=0;
+            if(sum<=0) sum=0;
         }
         return max;
+        
     }
 }
