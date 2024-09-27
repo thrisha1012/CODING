c4b4a15aef4187f4f56ba7803a14093e44653415
THRISHA S <126604173+thrisha1012@users.noreply.github.com>
2024-09-27 16:01:30 +0530

    Time: 3 ms (47.74%), Space: 44.6 MB (75.39%) - LeetHub

--- 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/1463-the-k-weakest-rows-in-a-matrix.java
@@ -15,7 +15,7 @@ class Solution {
         }
         for(int i=0;i<r-1;i++){
             for(int j=0;j<r-i-1;j++){
-                if(arr[j][0]>arr[j+1][0] || arr[j][0]==arr[j+1][0] && arr[j][1]>arr[j+1][1]){
+                if(arr[j][0]>arr[j+1][0]){
                     int[] temp=arr[j];
                     arr[j]=arr[j+1];
                     arr[j+1]=temp;
