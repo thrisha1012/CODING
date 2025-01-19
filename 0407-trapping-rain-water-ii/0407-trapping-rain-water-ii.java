9b04db389f1e571de4b2a03b366f3e787bb8dceb
THRISHA S <thrishamani2004@gmail.com>
2025-01-19 16:51:06 +0530

    Time: 19 ms (68.72%), Space: 46.2 MB (85.1%) - LeetHub

--- 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
+++ 0407-trapping-rain-water-ii/0407-trapping-rain-water-ii.java
@@ -7,7 +7,6 @@ class Solution {
 
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->a[0]-b[0]);
 
-        //Set the boundary element as visited
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(i==0 || i==m-1 || j==0 || j==n-1){
@@ -17,27 +16,18 @@ class Solution {
             }
         }
 
-
         int[][] direction={ {0,1}, {0,-1}, {1,0}, {-1,0} };
 
         int waterVolume=0;
-
-        //Applying the BFS Traversal
         while(!pq.isEmpty()){
             int[] arr=pq.poll();
-            int cv=arr[0];    //Curr value
-            int cr=arr[1];    //Curr row
-            int cc=arr[2];    //Curr column
-
-            //Visiting the adjacent elemetns of current element
+            int cv=arr[0];    
+            int cr=arr[1];    
+            int cc=arr[2];   
             for(int[] dir:direction){
-                int nr=cr+dir[0];   //New row
-                int nc=cc+dir[1];   //New column    
-
-                // Checking the element is within row, column and not visited
+                int nr=cr+dir[0];
+                int nc=cc+dir[1];  
                 if(nr>=0 && nr<m && nc>=0 && nc<n && !visited[nr][nc]){
-                    
-                    //volume of water it can trap after raining.
                     if(cv-heightMap[nr][nc]>0){
                         waterVolume+=cv-heightMap[nr][nc];
                         pq.add(new int[]{cv, nr, nc});
