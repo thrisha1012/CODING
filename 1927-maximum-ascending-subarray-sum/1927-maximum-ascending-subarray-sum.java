ffec1dda56a313bc9b5662cd9b5c587b79a034d2
THRISHA S <thrishamani2004@gmail.com>
2025-02-04 11:03:31 +0530

    Time: 0 ms (100%), Space: 41.1 MB (92.58%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxAscendingSum(int[] nums) {
         int n=nums.length;
         int currsum=nums[0];
-        int maxsum=currsum;
+        int maxsum=nums[0];
         for(int i=1;i<n;i++){
             if(nums[i]>nums[i-1]){
                 currsum=currsum+nums[i];
